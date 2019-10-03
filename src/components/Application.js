94608546010eb2621c9fa2cb18434e30d97c0869
SarahMahovlich <sarah.mahov@gmail.com>
2019-10-02 21:28:42 -0700

    Add useVisualMode to Appointment Component

--- src/components/Application.js
+++ src/components/Application.js
@@ -25,7 +25,8 @@ export default function Application(props) {
       Promise.resolve(axios.get(`/api/interviewers`))
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
+    })
+    .catch(err => console.log(err));
   }, []);
   
   return (
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,42 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 import "./styles.scss";
 
-export default function Appointment({time, id, interview}) {
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+export default function Appointment({ time, id, interview, interviewers, onAdd, onSave, onCancel }) {
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  function onAdd() {
+    transition(CREATE);
+  }
+
+  function onCancel() {
+    back();
+  }
+
+ interviewers = [];
+  
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={interviewers} onCancel={onCancel} />}
     </article>
   );
 }
+
